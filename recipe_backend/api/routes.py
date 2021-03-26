686caf29dc5f51035bfaedc29e5bc4b725ca1ffb
loressl <loryssl@hotmail.com>
2021-03-26 00:25:01 -0300

    Adiciona rotas.

--- recipe_backend/api/routes.py
+++ recipe_backend/api/routes.py
@@ -1,6 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from .views import *
+
 routers = DefaultRouter()
+routers.register("users", ChefUserViewSet)
+routers.register("recipes", RecipeViewSet)
 
 urlpatterns =[path("", include(routers.urls))]
