c4d4ba5f9834f9ee55dcfac0ca110863e0922793
loressl <loryssl@hotmail.com>
2021-03-26 00:27:33 -0300

    Adiciona rotas que retornam o token.

--- recipe_backend/recipe_backend/urls.py
+++ recipe_backend/recipe_backend/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include("api.routes"), name="api")
+    path('api/', include("api.routes"), name="api"),
+    path('api/token/', TokenObtainPairView.as_view(), name= "token_obtain_pair"),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
 ]
