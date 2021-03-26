c23e801fb4bcc07c7194cd02a6c078af9eb6d9b6
loressl <loryssl@hotmail.com>
2021-03-26 00:22:25 -0300

    Adiciona classes para representar no admin.

--- recipe_backend/api/admin.py
+++ recipe_backend/api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+class ChefUserAdminConfig(UserAdmin):
+    model = ChefUser
+    search_fields = ('email', 'first_name', 'username')
+    list_display= ('username', 'email', 'first_name', 'last_name')
+    fieldsets = UserAdmin.fieldsets
+
+class RecipeAdminConfig(admin.ModelAdmin):
+    model = Recipe
+    list_display=('title', 'chef')
+
+admin.site.register(ChefUser,ChefUserAdminConfig)
+admin.site.register(Recipe, RecipeAdminConfig)
