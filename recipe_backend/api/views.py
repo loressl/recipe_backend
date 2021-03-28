41352073b90cb0a1b1bef241cc9c61d838513a1c
loressl <loryssl@hotmail.com>
2021-03-28 01:05:53 -0300

    Adiciona rotas para gerenciar as receitas. Coloca permissons nas rotas.

--- recipe_backend/api/views.py
+++ recipe_backend/api/views.py
@@ -2,9 +2,11 @@ from rest_framework import viewsets, status
 from rest_framework.views import Response
 from .models import *
 from .serializers import *
-from rest_framework.permissions import  SAFE_METHODS,AllowAny, IsAuthenticatedOrReadOnly, BasePermission
+from rest_framework.permissions import SAFE_METHODS, AllowAny, IsAuthenticatedOrReadOnly, BasePermission
+from rest_framework.decorators import action
 
 # Create your views here.
+
 
 class RecipePermission(BasePermission):
     message = 'Edição das receitas é restrita para os cadastrados.'
@@ -24,7 +26,7 @@ class ChefUserViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         try:
             ChefUser.objects.get(email=request.data['email'])
-            return Response({"mensagem": "Usuário já está cadastrado. Verifique seus dados"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"mensagem": "Usuário já está cadastrado. Verifique seus dados"}, status=status.HTTP_404_NOT_FOUND)
         except ChefUser.DoesNotExist:
             pass
         serializer = self.get_serializer(data=request.data)
@@ -57,20 +59,78 @@ class ChefUserViewSet(viewsets.ModelViewSet):
 class RecipeViewSet(viewsets.ModelViewSet):
 
     queryset = Recipe.objects.all()
-    serializer = RecipeSerializer
+    serializer_class = RecipeSerializer
     lookup_field = "id"
 
     def get_permissions(self):
-        if self.action == 'list':
+        if self.action == 'list' or self.action == 'recipe-detail' or self.action == 'search_recipe' or self.action == 'recipes_chef':
             self.permission_classes = [AllowAny]
         else:
             self.permission_classes = [IsAuthenticatedOrReadOnly]
         return super(self.__class__, self).get_permissions()
 
     def create(self, request):
-        print(request.data)
-        return Response({}, status=status.HTTP_200_OK)
+        try:
+            Recipe.objects.get(title=request.data['title'])
+            return Response({"mensagem": "Usuário já está cadastrado. Verifique seus dados"}, status=status.HTTP_400_BAD_REQUEST)
+        except Recipe.DoesNotExist:
+            pass
+        chefUser = ChefUser.objects.get(email=request.user)
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(chef=chefUser)
+            return Response({"mensagem": "Receita cadastrada com sucesso!!"}, status=status.HTTP_201_CREATED)
+        return Response({"mensagem": "Ocorreu um erro. Verifique seus dados!!"}, status=status.HTTP_400_BAD_REQUEST)
 
-    def list(self, request, *args, **kwargs):
-        print("deu certo")
-        return Response({}, status=status.HTTP_200_OK)
+    def list(self, request, id=None):
+        recipes = self.get_queryset()
+        if not recipes:
+            return Response({"mensagem": "Ainda não há receitas cadastrada na base de dados."}, status=status.HTTP_404_NOT_FOUND)
+        page = self.paginate_queryset(recipes)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(recipes, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, id=None):
+        instance = self.get_object()
+        seliarizer = self.get_serializer(instance)
+        return Response(seliarizer.data, status=status.HTTP_200_OK)
+
+    def update(self, request, id=None):
+        partial = True
+        instance = self.get_object()
+        serializer = self.get_serializer(
+            instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(methods=['post'], detail=True, url_path='search-recipe', url_name='search_recipe', permission_classes=[AllowAny])
+    def search_recipe(self, request):
+        result_title = Recipe.objects.filter(
+            title__icontains=request.data['search'])
+        result_ingredients = Recipe.objects.filter(
+            ingredients__icontains=request.data['search'])
+        result_final = list(set(result_title) | set(result_ingredients))
+        if not result_final:
+            return Response({"mensagem": "Não existe receitas para essa busca."}, status=status.HTTP_404_NOT_FOUND)
+        serializer = self.get_serializer(result_final, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(methods=['post'], detail=True,  url_path='recipes-chef', url_name='recipes_chef', permission_classes=[AllowAny])
+    def recipes_chef(self, request):
+        #verificar se vem dois nomes ou um para procurar pelo segundo nome
+        chef = ChefUser.objects.filter(first_name__icontains=request.data['search'])
+        result_recipes = Recipe.objects.filter(chef= chef[0].id)
+        if not result_recipes:
+            return Response({"mensagem": "Não existe receitas para essa busca."}, status=status.HTTP_404_NOT_FOUND)
+        serializer = self.get_serializer(result_recipes, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
